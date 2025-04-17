d4f39ec18942efae7798cfd9b436445da2e20d25
Zain Abubaker <zainabubaker2002@gmail.com>
2025-04-18 01:14:57 +0300

    nth important

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -6,14 +6,17 @@ const User = require('../models/User');
 exports.register = async (req, res) => {
   const { name, email, password } = req.body;
 
+
   try {
     const exists = await User.findOne({ email });
     if (exists) return res.status(400).json({ message: 'Email already in use' });
 
     const hashed = await bcrypt.hash(password, 10);
     await User.create({ name, email, password: hashed });
+    console.log('REGISTER BODY:', req.body);
 
     return res.status(201).json({ message: 'User created' });
+    
   } catch (err) {
     return res.status(500).json({ message: 'Server error', error: err.message });
   }
